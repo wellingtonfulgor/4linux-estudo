ea19799ef86d7ebafd38d8170c189197a60fa856
wellingtonfulgor <wellingtonfulgor@gmail.com>
2019-11-14 22:51:49 -0200

    aula04 comportamento

--- aula04/serializacao/comportamento.php
+++ aula04/serializacao/comportamento.php
@@ -1,1 +1,19 @@
 <?php
+
+class VirarString{
+    public function __construct($mensagem){
+        $this->foo = $mensagem;
+    }
+
+    public function __toString(){
+        return 'Eae mano! Quero virar string nao<br>';
+    }
+
+    public function __invoke(){
+        echo '4 dias de curso e nao aprendeu ainda?';
+    }
+}
+
+$obj = new VirarString('oi');
+echo $obj;
+$obj(45);
