eaaf5da014d127cbd056e6190ea66f35848595c7
wellingtonfulgor <wellingtonfulgor@gmail.com>
2019-11-18 20:40:36 -0200

    aula 05 set

--- aula05/set.php
+++ aula05/set.php
@@ -1,12 +1,45 @@
 <?php
+
 class TestePropriedade{
+    protected $nome;
+
     public function __set($propriedade, $valor){
         if(property_exists(__CLASS__, $propriedade )){
             $this->propriedade = $valor;
         }else{
-            echo "propriedade nao existe";
+            echo "propriedade {$propriedade} nao existe<br>";
+            echo "O valor: {$valor} Nao pode ser criado no momento";
         }
     }
 }
 
-$obj = new TestePropriedade;
+
+class TestePropriedade2{
+    protected $nome;
+    protected $idade;
+    protected $sobrenome;
+
+    public function __construct(string $nome, int $idade){
+        $this->nome = $nome;
+        $this->idate = $idade;
+    
+    }
+
+    public function __set($propriedade, $valor){
+        if(property_exists(__CLASS__, $propriedade)){
+            echo "Valor Atribuido";
+            $this->$propriedade = $valor;
+        }else{
+            echo "Voce nao possui permissao para criar atributos<br>";
+       }       
+    }
+        
+ 
+
+    public function __get($propriedade){
+        echo "Voce acessou o atributo {$propriedade}<br>";
+    }
+}
+
+$obj2 = new TestePropriedade2('Wellington', 20);
+echo $obj2->nome;
